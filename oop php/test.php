3ecd14f69c6c2de23b2451516f017da7cfbdbbfd
zhenja666 <61518173+zhenja666@users.noreply.github.com>
2020-02-26 21:34:30 +0200

    Add files via upload

---
+++ oop php/test.php
@@ -0,0 +1,14 @@
+<?php
+function name (){
+    echo "показало сука ";
+}
+
+
+function surName (){
+    $array = array('йобана','хуйня','сука');
+    echo $array[1];
+}
+print name(). surName ();
+
+
+?>
